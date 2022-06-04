465410fd97ed74ece6b22620f960dc3a5ad4a339
Irwin Schmitt <irwinschmitt@gmail.com>
2022-06-04 16:02:57 -0300

    style: add jsx-sort-props rule

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -74,8 +74,8 @@ MyDocument.getInitialProps = async (ctx) => {
   const emotionStyles = extractCriticalToChunks(initialProps.html);
   const emotionStyleTags = emotionStyles.styles.map((style) => (
     <style
-      data-emotion={`${style.key} ${style.ids.join(" ")}`}
       key={style.key}
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
       // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: style.css }}
     />
